ef2fd1c863f8fb9e38093b2e5e90f27e8580ed9f
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-04-24 22:32:14 +0600

    LoginActivity design finished

---
+++ app/src/main/java/com/treebricks/hrmanagement/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.treebricks.hrmanagement;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+}
