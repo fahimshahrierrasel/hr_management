b4c4090f3afef4680bac8a6d2965a206364a3ff8
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-05-03 05:43:20 +0600

    Leave Application Details add

--- app/src/main/java/com/treebricks/hrmanagement/adapters/LeaveApplicationRecyclerViewAdapter.java
+++ app/src/main/java/com/treebricks/hrmanagement/adapters/LeaveApplicationRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package com.treebricks.hrmanagement.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import com.treebricks.hrmanagement.R;
+import com.treebricks.hrmanagement.fragments.LeaveApplicationDetailsFragment;
 import com.treebricks.hrmanagement.models.ApplicationItem;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -35,6 +37,16 @@ public class LeaveApplicationRecyclerViewAdapter extends RecyclerView.Adapter<Le
         holder.tvLeaveApplicantName.setText(applicationItem.getApplicantName());
         holder.tvLeaveType.setText(applicationItem.getLeaveType());
         holder.tvTotalLeaveDays.setText(String.format(Locale.US,"%d days", applicationItem.getTotalLeave()));
+        holder.cvContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((AppCompatActivity) context).getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_placeholder, new LeaveApplicationDetailsFragment(), null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 
     @Override
@@ -43,14 +55,14 @@ public class LeaveApplicationRecyclerViewAdapter extends RecyclerView.Adapter<Le
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
-        CardView cvLeaveApplicationItem;
+        CardView cvContainer;
         TextView tvLeaveApplicantName;
         TextView tvLeaveType;
         TextView tvTotalLeaveDays;
 
         ViewHolder(View itemView) {
             super(itemView);
-            cvLeaveApplicationItem = itemView.findViewById(R.id.cvLeaveApplicationItem);
+            cvContainer = itemView.findViewById(R.id.cvLeaveApplicationItem);
             tvLeaveApplicantName = itemView.findViewById(R.id.tv_employee_name);
             tvLeaveType = itemView.findViewById(R.id.tvLeaveType);
             tvTotalLeaveDays = itemView.findViewById(R.id.tvTotalLeaveDays);
---
+++ app/src/main/java/com/treebricks/hrmanagement/fragments/LeaveApplicationDetailsFragment.java
@@ -0,0 +1,30 @@
+package com.treebricks.hrmanagement.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.treebricks.hrmanagement.R;
+import com.treebricks.hrmanagement.adapters.ProfileContentRecyclerViewAdapter;
+import com.treebricks.hrmanagement.models.ProfileContent;
+
+import java.util.ArrayList;
+
+public class LeaveApplicationDetailsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_leave_application_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
